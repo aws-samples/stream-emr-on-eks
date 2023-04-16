adeeda9cc71cede1e85b1f2d2a110b96fe8cb3b7
Melody <meloyang@amazon.com>
2023-04-16 14:42:56 +0800

    move lf stack to the front, remove sleep in emr on ec2 step

--- app.py
+++ app.py
@@ -17,14 +17,12 @@ emr_release_v=app.node.try_get_context('emr_version')
 eks_stack = SparkOnEksStack(app, proj_name, proj_name)
 msk_stack = MSKStack(eks_stack,'kafka', proj_name, eks_stack.eksvpc)
 airflow_stack = AirflowStack(eks_stack, "AirflowStack", eks_stack.eksvpc, "emr-serverless-airflow")
-
-# 2.setup EMR on EC2
-emr_ec2_stack = EMREC2Stack(eks_stack, 'emr-on-ec2', emr_release_v, proj_name, eks_stack.eksvpc, eks_stack.code_bucket, eks_stack.LFEngineerRole, eks_stack.LFAnalystRole)
-# 3.setup Sagemaker notebook
-sagemaker_nb_stack = NotebookStack(eks_stack, 'sm_notebook', emr_ec2_stack.livy_sg, eks_stack.eksvpc, eks_stack.LFSagemakerRole, eks_stack.code_bucket)
-# 4.setup Lakeformation
+# 2.setup Lakeformation
 lf_stack = LFStack(eks_stack, 'lake_formation',eks_stack.LFEngineerRole,eks_stack.LFAnalystRole)
-
+# 3.setup EMR on EC2
+emr_ec2_stack = EMREC2Stack(eks_stack, 'emr-on-ec2', emr_release_v, proj_name, eks_stack.eksvpc, eks_stack.code_bucket, eks_stack.LFEngineerRole, eks_stack.LFAnalystRole)
+# 4.setup Sagemaker notebook
+sagemaker_nb_stack = NotebookStack(eks_stack, 'sm_notebook', emr_ec2_stack.livy_sg, eks_stack.eksvpc, eks_stack.LFSagemakerRole, eks_stack.code_bucket)
 
 Tags.of(eks_stack).add('project', proj_name)
 Tags.of(msk_stack).add('project', proj_name)
