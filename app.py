e1d014462b9f89493e04f9f419d3460840e17014
Melody <meloyang@amazon.com>
2023-04-17 02:15:29 +0800

    reorder the main stack and rest of nested stacks in app.py

--- app.py
+++ app.py
@@ -15,12 +15,10 @@ emr_release_v=app.node.try_get_context('emr_version')
 
 # 1.main stacks
 eks_stack = SparkOnEksStack(app, proj_name, proj_name)
-# 2.setup Lakeformation
-lf_stack = LFStack(eks_stack, 'lake_formation',eks_stack.LFEngineerRole,eks_stack.datalake_bucket)
-# 3.setup EMR on EC2
-emr_ec2_stack = EMREC2Stack(eks_stack, 'emr-on-ec2', emr_release_v, proj_name, eks_stack.eksvpc, eks_stack.code_bucket, eks_stack.LFEngineerRole, eks_stack.LFAnalystRole,eks_stack.datalake_bucket.bucket_name)
-# 4. the rest of nested stacks
 msk_stack = MSKStack(eks_stack,'kafka', proj_name, eks_stack.eksvpc)
+emr_ec2_stack = EMREC2Stack(eks_stack, 'emr-on-ec2', emr_release_v, proj_name, eks_stack.eksvpc, eks_stack.code_bucket, eks_stack.LFEngineerRole, eks_stack.LFAnalystRole,eks_stack.datalake_bucket.bucket_name)
+# 2. the rest of nested stacks
+lf_stack = LFStack(eks_stack, 'lake_formation',eks_stack.LFEngineerRole,eks_stack.datalake_bucket)
 sagemaker_nb_stack = NotebookStack(eks_stack, 'sm_notebook', emr_ec2_stack.livy_sg, eks_stack.eksvpc, eks_stack.LFSagemakerRole, eks_stack.code_bucket)
 airflow_stack = AirflowStack(eks_stack, "AirflowStack", eks_stack.eksvpc, "emr-serverless-airflow")
 
